d35e95b09a23f42723099b7480c3026ef3f96c05
Leonardo <ssbmplayer@gmail.com>
2018-03-03 01:34:14 +0200

    Change for better loading

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,21 +1,32 @@
 // Right side loader
 
 function updateData(section){
+
+    document.getElementById("data").style.display = "none";
+    document.getElementById("ldr").style.display = "block";
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if (xhttp.readyState === 4) {
             if (xhttp.status === 200){
-    document.getElementById("data").innerHTML = xhttp.responseText;
-window.history.pushState("wow data", "Title", "/"+section);
-MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-}
-}
-}
-xhttp.open("GET", "/request/" + section, true);
-xhttp.send();
+                document.getElementById("data").innerHTML = xhttp.responseText;
+                document.getElementById("ldr").style.display = "none";
+                document.getElementById("data").style.display = "block";
+                window.history.pushState("wow data", "Title", "/"+section);
+                MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+        }
+        }
+        }
+        xhttp.open("GET", "/request/" + section, true);
+        xhttp.send();
         $('html,body').animate({
-        scrollTop: $("#data").offset().top
+        scrollTop: $("#ldr").offset().top
 }, 800);
+}
+
+function gosection() {
+    $('html,body').animate({
+        scrollTop: $("#data").offset().top
+    }, 800);
 }
 
 // ConverTex Scripts
@@ -27,6 +38,7 @@ function update() {
         scrollTop: $("#textMJ").offset().top
     }, 800);
 }
+
 function dls(a) {
         document.getElementById("datamj").value = document.getElementById("datamj").value + a;
         document.getElementById("datamj").focus();
