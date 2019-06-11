467f5c62a53c5767f2677c60c86bf7e50e48bf26
Leonardo Martínez <ssbmplayer@gmail.com>
2019-06-11 11:08:19 +0200

    Favicon, CV Update

--- create_plots.py
+++ create_plots.py
@@ -1,52 +1,47 @@
-    import pandas as pd
-    import matplotlib as mpl
-    import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-    #Utility to edit the color of plot
+#Utility to edit the color of plot
 
-    def change_color(color):
-        mpl.rcParams['text.color'] = color
-        mpl.rcParams['axes.labelcolor'] = color
-        mpl.rcParams['axes.edgecolor'] = color
-        mpl.rcParams['xtick.color'] = color
-        mpl.rcParams['ytick.color'] = color
-    change_color("#00002B")
+def change_color(color):
+    mpl.rcParams['text.color'] = color
+    mpl.rcParams['axes.labelcolor'] = color
+    mpl.rcParams['axes.edgecolor'] = color
+    mpl.rcParams['xtick.color'] = color
+    mpl.rcParams['ytick.color'] = color
+change_color("#00002B")
 
-    #Reads the CV
-    df=pd.read_excel('data/CV.xlsx',sheet_name="ScientificPublication",header=1,skiprows=0)
-    df.date=pd.to_numeric(df.date)
-    print(df.info())
+#Reads the CV
+df=pd.read_excel('data/CV.xlsx',sheet_name="ScientificPublication",header=1,skiprows=0)
+df.date=pd.to_numeric(df.date)
+print(df.info())
 
-    #Masks for selecting only papers or proceedings
-    papers_mask=df['status']=="Journal"
-    proc_mask=df['status']=="Proceedings"
-    preprint_mask=df['status']=='Preprint'
+#Masks for selecting only papers or proceedings
+papers_mask=df['status']=="Journal"
+proc_mask=df['status']=="Proceedings"
+preprint_mask=df['status']=='Preprint'
 
-    #Create the dataframes for papers and proceedings
-    papers=df[papers_mask]
-    proc=df[proc_mask]
-    preprints=df[preprint_mask]
+#Create the dataframes for papers and proceedings
+papers=df[papers_mask]
+proc=df[proc_mask]
+preprints=df[preprint_mask]
 
-    #Count per year
-    years=range(2011,2020,1)
-    papers_peryear=papers.groupby('date').count().reindex(years, fill_value=0)
-    proc_peryear=proc.groupby('date').count().reindex(years, fill_value=0)
-    preprint_peryear=preprints.groupby('date').count().reindex(years, fill_value=0)
+#Count per year
+years=range(2013,2020,1)
+papers_peryear=papers.groupby('date').count().reindex(years, fill_value=0)
+proc_peryear=proc.groupby('date').count().reindex(years, fill_value=0)
+preprint_peryear=preprints.groupby('date').count().reindex(years, fill_value=0)
 
-    #Create the plot
-    # plt.bar(years,papers_peryear['id'], label='Journal Papers', color="#C83771")
-    # plt.bar(years,proc_peryear['id'], bottom=papers_peryear['id'], label='Proceedings', color="#00002B")
-    # plt.bar(years,preprint_peryear['id'], bottom=papers_peryear['id']+proc_peryear['id'], label='Preprints', color="#6c757d")
+#Create the plot
+plt.bar(years,papers_peryear['id'], label='Journal Papers', color="#C83771")
+plt.bar(years,proc_peryear['id'], bottom=papers_peryear['id'], label='Proceedings', color="#00002B")
+plt.bar(years,preprint_peryear['id'], bottom=papers_peryear['id']+proc_peryear['id'], label='Preprints', color="#6c757d")
 
-    plt.bar(years,papers_peryear['id'], label='Journal Papers')
-    plt.bar(years,proc_peryear['id'], bottom=papers_peryear['id'], label='Proceedings')
-    plt.bar(years,preprint_peryear['id'], bottom=papers_peryear['id']+proc_peryear['id'], label='Preprints')
-
-    plt.colors
-    plt.xticks(years)
-    plt.xlabel('Year')
-    plt.ylabel('Count')
-    plt.legend(loc='best',facecolor="white")
-    plt.gca().set_aspect(0.8)
-    # plt.savefig('papers.png',dpi=120,transparent=True)
-    plt.show()
+plt.xticks(years)
+plt.xlabel('Year')
+plt.ylabel('Count')
+plt.legend(loc='best',facecolor="white")
+plt.gca().set_aspect(0.8)
+plt.savefig('papers{}.png'.format(years),dpi=120,transparent=True)
+plt.show()
