1d84024ca34155d880ecff380207ce0af656874c
Leonardo Martínez <ssbmplayer@gmail.com>
2020-12-07 10:02:27 -0600

    Openblas

--- cvutils/__init__.py
+++ cvutils/__init__.py
@@ -1,5 +1,7 @@
 # Tool to handle CVs
 # Created by Leonardo Ignacio Martínez Sandoval
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
 # openpyxl helps to work with MSExcel databases
 import openpyxl as opx
